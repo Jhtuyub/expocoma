203b3788b4cdd81ee43bc5de8b9d06a53fc5a446
jhtuyub <isctuyubjhonata@gmail.com>
2017-04-03 11:30:09 -0500

    agregar arti vista
    Se agrego filtro

--- EXPOCOMA/Stand/FrmAgregarProveArti.Designer.cs
+++ EXPOCOMA/Stand/FrmAgregarProveArti.Designer.cs
@@ -34,23 +34,30 @@
             this.dgvProveedor = new System.Windows.Forms.DataGridView();
             this.btnGuardar = new System.Windows.Forms.Button();
             this.btnSalir = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProveedor)).BeginInit();
             this.SuspendLayout();
             // 
             // txtClProve
             // 
             this.txtClProve.Enabled = false;
-            this.txtClProve.Location = new System.Drawing.Point(12, 12);
+            this.txtClProve.Location = new System.Drawing.Point(12, 29);
             this.txtClProve.Name = "txtClProve";
             this.txtClProve.Size = new System.Drawing.Size(100, 25);
             this.txtClProve.TabIndex = 0;
             // 
             // txtNomProve
             // 
+            this.txtNomProve.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.txtNomProve.Enabled = false;
-            this.txtNomProve.Location = new System.Drawing.Point(118, 12);
+            this.txtNomProve.Location = new System.Drawing.Point(118, 29);
             this.txtNomProve.Name = "txtNomProve";
-            this.txtNomProve.Size = new System.Drawing.Size(592, 25);
+            this.txtNomProve.Size = new System.Drawing.Size(624, 25);
             this.txtNomProve.TabIndex = 1;
             // 
             // dgvProveedor
@@ -63,11 +70,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvProveedor.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvProveedor.BackgroundColor = System.Drawing.Color.White;
-            this.dgvProveedor.Location = new System.Drawing.Point(12, 43);
+            this.dgvProveedor.Location = new System.Drawing.Point(12, 108);
             this.dgvProveedor.Name = "dgvProveedor";
             this.dgvProveedor.RowHeadersWidth = 4;
             this.dgvProveedor.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvProveedor.Size = new System.Drawing.Size(698, 227);
+            this.dgvProveedor.Size = new System.Drawing.Size(730, 199);
             this.dgvProveedor.TabIndex = 2;
             this.dgvProveedor.TabStop = false;
             this.dgvProveedor.Tag = "";
@@ -75,7 +82,7 @@
             // btnGuardar
             // 
             this.btnGuardar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnGuardar.Location = new System.Drawing.Point(544, 276);
+            this.btnGuardar.Location = new System.Drawing.Point(576, 313);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(80, 29);
             this.btnGuardar.TabIndex = 3;
@@ -86,7 +93,7 @@
             // btnSalir
             // 
             this.btnSalir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSalir.Location = new System.Drawing.Point(630, 276);
+            this.btnSalir.Location = new System.Drawing.Point(662, 313);
             this.btnSalir.Name = "btnSalir";
             this.btnSalir.Size = new System.Drawing.Size(80, 29);
             this.btnSalir.TabIndex = 4;
@@ -94,11 +101,61 @@
             this.btnSalir.UseVisualStyleBackColor = true;
             this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(536, 77);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(206, 25);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(518, 25);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Clave:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Nombre Proveedor:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Buscar:";
+            // 
             // FrmAgregarProveArti
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(722, 317);
+            this.ClientSize = new System.Drawing.Size(754, 354);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.btnGuardar);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.dgvProveedor);
@@ -128,5 +185,10 @@
         private System.Windows.Forms.DataGridView dgvProveedor;
         private System.Windows.Forms.Button btnGuardar;
         private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
