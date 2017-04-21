709c7267bf60cd0c43178f760b662d88f6f94a83
jhtuyub <isctuyubjhonata@gmail.com>
2017-04-21 12:39:47 -0500

    CODIGO - FrmImprimirInvita
    Programación para acumular los datos con ruta de baja para que con un
    boton genere un excel con la información del los clientes

--- EXPOCOMA/Stand/FrmImprimirInvita.cs
+++ EXPOCOMA/Stand/FrmImprimirInvita.cs
@@ -1,11 +1,13 @@
 ﻿using BarcodeFree;
 using CrystalDecisions.CrystalReports.Engine;
 using EXPOCOMA.reportes;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -23,9 +25,12 @@ namespace EXPOCOMA.Stand
         DataTable _dtClientes;
         DataTable _dtCliInvitaciones;
 
+        DataTable _dtCliBaja = new DataTable();
+
         DSReportes DSReport;
-        IEnumerable<DataRow> sql_dtProve;
+        IEnumerable<DataRow> sql_dtClien;
         IEnumerable<DataRow> sql_dtClieInv;
+        IEnumerable<DataRow> sql_dtsucusal;
 
         private Thread ThreadImprimir;
 
@@ -50,6 +55,12 @@ namespace EXPOCOMA.Stand
             cBoxSucursal.DataSource = _dtSucursales;
             cBoxSucursal.ValueMember = "ALMACEN";//"valor";
             cBoxSucursal.DisplayMember = "SUCURSAL"; //"opcion";
+
+            _dtCliBaja.Columns.Add("C_CLIENTE", typeof(String));
+            _dtCliBaja.Columns.Add("NOM_CLIEN", typeof(String));
+            _dtCliBaja.Columns.Add("CLIAGEN", typeof(String));
+            _dtCliBaja.Columns.Add("NOM_AGENTE", typeof(String));
+            _dtCliBaja.Columns.Add("C_RUTA", typeof(String));
 
             //_frmParent = this;
 
@@ -88,21 +99,37 @@ namespace EXPOCOMA.Stand
             Code128 text = new Code128();
             String[] clvCliente;
             String clvNoCliente = "";
+            String ClieRutaBaja = "";
+            //String ClieAgenBaja = "";
             String claves = txtClvClientes.Text.Trim();
+            String rutaBaja="";
+            String agenBaja="";
             DSReport = new DSReportes();
-            String idSucur="";
+            String idSucur = "";
+            _dtCliBaja.Clear();
+            
             this.Invoke((MethodInvoker)delegate
             {
-                 idSucur= cBoxSucursal.SelectedValue.ToString();
+                idSucur = cBoxSucursal.SelectedValue.ToString();
             });
-            
+
 
             //            SELECT clie.ID_SUCURSALALM, clie.C_CLIENTE, clie.NOM_CLIEN, clie.C_AGENTE, agen.C_AGENTE, agen.NOM_AGENTE
             //FROM         dbf_cliente clie, dbf_agentes agen
             //WHERE(clie.C_AGENTE = agen.C_AGENTE)AND(clie.ID_SUCURSALALM = '001' AND agen.ID_SUCURSALALM = '001')
-            _dtClientes = _funcion.llenar_dt("dbf_cliente clie, dbf_agentes agen", "clie.ID_SUCURSALALM, clie.C_CLIENTE, clie.NOM_CLIEN, clie.C_AGENTE AS CLIAGEN, agen.C_AGENTE AS AGENAGEN, agen.NOM_AGENTE, clie.NOM_TIENDA, clie.POBLACION, clie.TELEFONO", "WHERE (clie.C_AGENTE = agen.C_AGENTE)AND(clie.ID_SUCURSALALM = '" + idSucur + "' AND agen.ID_SUCURSALALM = '" + idSucur + "')");// "WHERE ID_SUCURSALALM = "+cBoxSucursal.SelectedValue.ToString());
+            _dtClientes = _funcion.llenar_dt("dbf_cliente clie, dbf_agentes agen", "clie.ID_SUCURSALALM, clie.C_CLIENTE, clie.NOM_CLIEN, clie.C_AGENTE AS CLIAGEN, agen.C_AGENTE AS AGENAGEN, agen.NOM_AGENTE, clie.C_RUTA, clie.NOM_TIENDA, clie.POBLACION, clie.TELEFONO", "WHERE (clie.C_AGENTE = agen.C_AGENTE)AND(clie.ID_SUCURSALALM = '" + idSucur + "' AND agen.ID_SUCURSALALM = '" + idSucur + "')");// "WHERE ID_SUCURSALALM = "+cBoxSucursal.SelectedValue.ToString());
+            
 
-
+            sql_dtsucusal =
+                   from dtSucu in _dtSucursales.AsEnumerable()
+                   where dtSucu.Field<String>("ALMACEN") == idSucur
+                   select dtSucu;
+            foreach (DataRow filSucu in sql_dtsucusal)
+            {
+                rutaBaja = filSucu["RUTA_BAJA"].ToString();
+                agenBaja = filSucu["AGEN_BAJA"].ToString();
+                break;
+            }
 
 
             _dtClientes.Columns.Add("C_CLIEXPO", typeof(String));
@@ -127,23 +154,52 @@ namespace EXPOCOMA.Stand
             for (int i = 0; i < clvCliente.Count(); i++)
             {
 
-                sql_dtProve =
+                sql_dtClien =
                    from dtClien in _dtClientes.AsEnumerable()
                    where dtClien.Field<String>("C_CLIENTE") == clvCliente[i].ToString()
                    select dtClien;
 
-                if (sql_dtProve.Count() > 0)
+                if (sql_dtClien.Count() > 0)
                 {
-                    foreach (DataRow filClien in sql_dtProve)
+
+
+                    foreach (DataRow filClien in sql_dtClien)
                     {
-                        _dtCliInvitaciones = _funcion.llenar_dt("tbl_ctesinvitados", "*", "WHERE ID_SUCURSALALM = '" + idSucur + "'");
-                        //fil_dtClien = _dtClientes.Rows.Count;
-                        //fil_dtClien = filClien.Rows.Count;
-                        //for (int ii = 0; ii <= fil_dtClien-2; ii++)
+
+                        //MessageBox.Show(rutaBaja+"="+filClien["C_RUTA"].ToString() +"-"+ agenBaja+"="+filClien["CLIAGEN"].ToString());
+
+                        if (filClien["C_RUTA"].ToString() ==rutaBaja)
+                        {
+                            ClieRutaBaja += clvCliente[i].ToString() + "\r\n";
+                            _dtCliBaja.Rows.Add(
+                                new Object[]
+                                {
+                                    filClien["C_CLIENTE"].ToString(),
+                                    filClien["NOM_CLIEN"].ToString(),
+                                    filClien["CLIAGEN"].ToString(),
+                                    filClien["NOM_AGENTE"].ToString(),
+                                    filClien["C_RUTA"].ToString(),
+                                }
+                                );
+                        }
+                        //else if (filClien["CLIAGEN"].ToString() == agenBaja)
                         //{
-                        DSReport.Tables["dtInvitaciones"].Rows.Add(
-                            new Object[]
-                            {
+                        //    ClieAgenBaja += clvCliente[i].ToString() + "\r\n";
+                        //}
+                        else
+                        {
+
+
+
+
+                            _dtCliInvitaciones = _funcion.llenar_dt("tbl_ctesinvitados", "*", "WHERE ID_SUCURSALALM = '" + idSucur + "'");
+                            //fil_dtClien = _dtClientes.Rows.Count;
+                            //fil_dtClien = filClien.Rows.Count;
+                            //for (int ii = 0; ii <= fil_dtClien-2; ii++)
+                            //{
+                            DSReport.Tables["dtInvitaciones"].Rows.Add(
+                                new Object[]
+                                {
                                     filClien["C_CLIEXPO"].ToString(),
                                     filClien["NOM_CLIEN"].ToString(),
                                     filClien["NOM_TIENDA"].ToString(),
@@ -152,46 +208,53 @@ namespace EXPOCOMA.Stand
                                     filClien["AGENAGEN"].ToString(),
                                     filClien["NOM_AGENTE"].ToString(),
                                     filClien["BARCODE"].ToString(),
-                            }
-                            );
+                                }
+                                );
 
-                        sql_dtClieInv =
-                    from dtClieInv in _dtCliInvitaciones.AsEnumerable()
-                    where dtClieInv.Field<String>("C_CLIEXPO") == filClien["C_CLIEXPO"].ToString()
-                    select dtClieInv;
-                        //foreach (DataRow filClieInv in sql_dtClieInv)
-                        //{
-                        if (!(sql_dtClieInv.Count() > 0))
-                        {
-                            string[,] _datos = {
+                            sql_dtClieInv =
+                        from dtClieInv in _dtCliInvitaciones.AsEnumerable()
+                        where dtClieInv.Field<String>("C_CLIEXPO") == filClien["C_CLIEXPO"].ToString()
+                        select dtClieInv;
+                            //foreach (DataRow filClieInv in sql_dtClieInv)
+                            //{
+                            if (!(sql_dtClieInv.Count() > 0))
+                            {
+                                string[,] _datos = {
                                     {"ID_SUCURSALALM", "C_CLIENTE","C_CLIEXPO"},
                                     { filClien["ID_SUCURSALALM"].ToString(),  filClien["C_CLIENTE"].ToString(), filClien["C_CLIEXPO"].ToString()},
                                     {"varchar", "varchar", "varchar"}
                                 };
 
-                            //__funciones.validar_campo(this, _datos, "cat_expo", timMensaje, ___accion, ___idDato);
-                            _funcion.guardar_datos(_datos, "tbl_ctesinvitados", "nuevo", 0);
+                                //__funciones.validar_campo(this, _datos, "cat_expo", timMensaje, ___accion, ___idDato);
+                                _funcion.guardar_datos(_datos, "tbl_ctesinvitados", "nuevo", 0);
+                            }
+                            //}
+
+
+
+                            //}
+                            //    for (int i = 0; i <= filas - 2; i++)
+                            //    {
+                            //        dsReport.Tables[0].Rows.Add
+                            //            (
+                            //            new object[]
+                            //            {
+                            //   dtDatos.Rows[i]["C_CLIENTE"].ToString(),
+                            //   dtDatos.Rows[i]["NOM_CLIEN"].ToString(),
+                            //   dtDatos.Rows[i]["NOM_TIENDA"].ToString(),
+                            //   dtDatos.Rows[i]["POBLACION"].ToString(),
+                            //   dtDatos.Rows[i]["TELEFONO"].ToString(),
+                            //   dtDatos.Rows[i]["C_AGENTE"].ToString(),
+
+                            //            }
+                            //            );
+
+
+
+
+
                         }
-                        //}
 
-
-
-                        //}
-                        //    for (int i = 0; i <= filas - 2; i++)
-                        //    {
-                        //        dsReport.Tables[0].Rows.Add
-                        //            (
-                        //            new object[]
-                        //            {
-                        //   dtDatos.Rows[i]["C_CLIENTE"].ToString(),
-                        //   dtDatos.Rows[i]["NOM_CLIEN"].ToString(),
-                        //   dtDatos.Rows[i]["NOM_TIENDA"].ToString(),
-                        //   dtDatos.Rows[i]["POBLACION"].ToString(),
-                        //   dtDatos.Rows[i]["TELEFONO"].ToString(),
-                        //   dtDatos.Rows[i]["C_AGENTE"].ToString(),
-
-                        //            }
-                        //            );
                     }
 
                 }
@@ -217,6 +280,8 @@ namespace EXPOCOMA.Stand
             this.Invoke((MethodInvoker)delegate
             {
                 txtNoImpre.Text = clvNoCliente;
+                txtCliRutBaja.Text = ClieRutaBaja;
+                //txtClieAgenBaja.Text = ClieAgenBaja;
 
                 FrmVistaPrevia frmVistaPre = new FrmVistaPrevia();
                 frmVistaPre.MdiParent = this.MdiParent;
@@ -241,6 +306,71 @@ namespace EXPOCOMA.Stand
         {
             txtClvClientes.Text = "";
             txtNoImpre.Text = "";
+            txtCliRutBaja.Text = "";
+        }
+
+        private void btnGenerarxls_Click(object sender, EventArgs e)
+        {
+            if (_dtCliBaja.Rows.Count > 0)
+            {
+                try
+                {
+                    SaveFileDialog selectCarpeta = new SaveFileDialog();
+                    selectCarpeta.FileName = "Clientes_ruta_baja";
+                    selectCarpeta.Filter = "xls|*.xls";
+                    selectCarpeta.Title = "Guardar clientes ruta de baja";
+                    if (selectCarpeta.ShowDialog() == DialogResult.OK)
+                    {
+                        //textBox1.Text = selectCarpeta.FileName;
+                        File.Delete(selectCarpeta.FileName);
+                        string rutaArchivo = selectCarpeta.FileName;
+                        String[] col = { "A", "B", "C", "D", "E" };
+                        int row;
+                        using (ExcelPackage package = new ExcelPackage(new FileInfo(rutaArchivo)))
+                        {
+                            var worksheet = package.Workbook.Worksheets.Add("Contenido");
+                            worksheet.Cells["A1"].Value = "C_CLIENTE";
+                            worksheet.Cells["B1"].Value = "NOMBRE";
+                            worksheet.Cells["C1"].Value = "C_AGENTE";
+                            worksheet.Cells["D1"].Value = "NOMBRE AGENTE";
+                            worksheet.Cells["E1"].Value = "C_RUTA";
+
+
+
+                            for (int ii = 0; ii < _dtCliBaja.Rows.Count; ii++)
+                            {
+                                row = 0;
+                                for (int jj = 0; jj < _dtCliBaja.Columns.Count; jj++)
+                                {
+                                    row = ii + 2;
+                                    //MessageBox.Show(dtDatos.Rows[i][j].ToString());
+                                    worksheet.Cells[col[jj].ToString() + row].Value = _dtCliBaja.Rows[ii][jj].ToString();
+                                    //texto += dtDatos.Rows[i][j].ToString() + "\t";
+                                }
+                                //worksheet.Cells["A"+i].Value = dtDatos.Rows[i][j].ToString()
+                            }
+
+
+                            package.Save();
+                        }
+
+                    }
+                }
+                catch (Exception ee)
+                {
+
+                    MessageBox.Show(ee.Message, "¡Espera!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+           
+            
+           
+        }
+
+
+        private void txtClvClientes_Enter(object sender, EventArgs e)
+        {
+            txtClvClientes.SelectAll();
         }
     }
 }
