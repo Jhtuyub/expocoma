c8a07ffd990b93b17e316701590487b0c40eadc9
jhtuyub <isctuyubjhonata@gmail.com>
2017-04-21 12:40:47 -0500

    VISTA - FrmImprimirInvita
    Asignación de los indices para el tap en modo de ejecución

--- EXPOCOMA/Stand/FrmImprimirInvita.Designer.cs
+++ EXPOCOMA/Stand/FrmImprimirInvita.Designer.cs
@@ -38,20 +38,21 @@
             this.btnSalir = new System.Windows.Forms.Button();
             this.picbCargando = new System.Windows.Forms.PictureBox();
             this.btnLimpiar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCliRutBaja = new System.Windows.Forms.TextBox();
+            this.btnGenerarxls = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picbCargando)).BeginInit();
             this.SuspendLayout();
             // 
             // txtClvClientes
             // 
-            this.txtClvClientes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtClvClientes.Location = new System.Drawing.Point(13, 65);
+            this.txtClvClientes.Location = new System.Drawing.Point(13, 96);
             this.txtClvClientes.Multiline = true;
             this.txtClvClientes.Name = "txtClvClientes";
             this.txtClvClientes.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtClvClientes.Size = new System.Drawing.Size(202, 297);
-            this.txtClvClientes.TabIndex = 0;
+            this.txtClvClientes.Size = new System.Drawing.Size(163, 259);
+            this.txtClvClientes.TabIndex = 3;
+            this.txtClvClientes.Enter += new System.EventHandler(this.txtClvClientes_Enter);
             // 
             // label1
             // 
@@ -59,16 +60,16 @@
             this.label1.Location = new System.Drawing.Point(12, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(70, 17);
-            this.label1.TabIndex = 1;
+            this.label1.TabIndex = 0;
             this.label1.Text = "Sucursal:";
             // 
             // btnImprimir
             // 
             this.btnImprimir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnImprimir.Location = new System.Drawing.Point(260, 377);
+            this.btnImprimir.Location = new System.Drawing.Point(359, 390);
             this.btnImprimir.Name = "btnImprimir";
             this.btnImprimir.Size = new System.Drawing.Size(75, 29);
-            this.btnImprimir.TabIndex = 2;
+            this.btnImprimir.TabIndex = 10;
             this.btnImprimir.Text = "Imprimir";
             this.btnImprimir.UseVisualStyleBackColor = true;
             this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
@@ -76,10 +77,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Location = new System.Drawing.Point(12, 76);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(106, 17);
-            this.label2.TabIndex = 3;
+            this.label2.TabIndex = 2;
             this.label2.Text = "Clave Clientes:";
             // 
             // cBoxSucursal
@@ -90,37 +91,34 @@
             this.cBoxSucursal.FormattingEnabled = true;
             this.cBoxSucursal.Location = new System.Drawing.Point(88, 9);
             this.cBoxSucursal.Name = "cBoxSucursal";
-            this.cBoxSucursal.Size = new System.Drawing.Size(289, 25);
-            this.cBoxSucursal.TabIndex = 4;
+            this.cBoxSucursal.Size = new System.Drawing.Size(371, 25);
+            this.cBoxSucursal.TabIndex = 1;
             // 
             // txtNoImpre
             // 
-            this.txtNoImpre.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtNoImpre.Location = new System.Drawing.Point(231, 65);
+            this.txtNoImpre.Location = new System.Drawing.Point(183, 96);
             this.txtNoImpre.Multiline = true;
             this.txtNoImpre.Name = "txtNoImpre";
             this.txtNoImpre.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtNoImpre.Size = new System.Drawing.Size(202, 297);
+            this.txtNoImpre.Size = new System.Drawing.Size(163, 259);
             this.txtNoImpre.TabIndex = 5;
             // 
             // label3
             // 
-            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(227, 45);
+            this.label3.Location = new System.Drawing.Point(180, 76);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(150, 17);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Clientes no impresos:";
+            this.label3.Size = new System.Drawing.Size(166, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Clientes no encontratos:";
             // 
             // btnSalir
             // 
             this.btnSalir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSalir.Location = new System.Drawing.Point(358, 377);
+            this.btnSalir.Location = new System.Drawing.Point(440, 390);
             this.btnSalir.Name = "btnSalir";
             this.btnSalir.Size = new System.Drawing.Size(75, 29);
-            this.btnSalir.TabIndex = 7;
+            this.btnSalir.TabIndex = 11;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
             this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
@@ -128,7 +126,7 @@
             // picbCargando
             // 
             this.picbCargando.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.picbCargando.Location = new System.Drawing.Point(383, 12);
+            this.picbCargando.Location = new System.Drawing.Point(465, 12);
             this.picbCargando.Name = "picbCargando";
             this.picbCargando.Size = new System.Drawing.Size(50, 50);
             this.picbCargando.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -137,20 +135,52 @@
             // 
             // btnLimpiar
             // 
-            this.btnLimpiar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnLimpiar.Location = new System.Drawing.Point(12, 377);
+            this.btnLimpiar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnLimpiar.Location = new System.Drawing.Point(12, 390);
             this.btnLimpiar.Name = "btnLimpiar";
             this.btnLimpiar.Size = new System.Drawing.Size(75, 29);
-            this.btnLimpiar.TabIndex = 20;
+            this.btnLimpiar.TabIndex = 9;
             this.btnLimpiar.Text = "Limpiar";
             this.btnLimpiar.UseVisualStyleBackColor = true;
             this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(352, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Clientes Ruta Baja:";
+            // 
+            // txtCliRutBaja
+            // 
+            this.txtCliRutBaja.Location = new System.Drawing.Point(352, 96);
+            this.txtCliRutBaja.Multiline = true;
+            this.txtCliRutBaja.Name = "txtCliRutBaja";
+            this.txtCliRutBaja.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtCliRutBaja.Size = new System.Drawing.Size(163, 259);
+            this.txtCliRutBaja.TabIndex = 7;
+            // 
+            // btnGenerarxls
+            // 
+            this.btnGenerarxls.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGenerarxls.Location = new System.Drawing.Point(352, 361);
+            this.btnGenerarxls.Name = "btnGenerarxls";
+            this.btnGenerarxls.Size = new System.Drawing.Size(163, 23);
+            this.btnGenerarxls.TabIndex = 8;
+            this.btnGenerarxls.Text = "Generar Excel";
+            this.btnGenerarxls.UseVisualStyleBackColor = true;
+            this.btnGenerarxls.Click += new System.EventHandler(this.btnGenerarxls_Click);
             // 
             // FrmImprimirInvita
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(445, 418);
+            this.ClientSize = new System.Drawing.Size(527, 431);
+            this.Controls.Add(this.btnGenerarxls);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCliRutBaja);
             this.Controls.Add(this.btnLimpiar);
             this.Controls.Add(this.picbCargando);
             this.Controls.Add(this.btnSalir);
@@ -187,5 +217,8 @@
         private System.Windows.Forms.Button btnSalir;
         private System.Windows.Forms.PictureBox picbCargando;
         private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCliRutBaja;
+        private System.Windows.Forms.Button btnGenerarxls;
     }
 }
