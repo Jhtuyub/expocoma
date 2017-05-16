c414461b0bc084fab27cba728efb2df59cbf0d97
jhtuyub <isctuyubjhonata@gmail.com>
2017-05-16 18:02:52 -0500

    CODIGÓ - FrmLogin
    validación del usuario al iniciar sesion para que continue el sistema

--- EXPOCOMA/login/FrmLogin.Designer.cs
+++ EXPOCOMA/login/FrmLogin.Designer.cs
@@ -28,21 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.SStripLogin = new System.Windows.Forms.StatusStrip();
+            this.stripPBEstatus = new System.Windows.Forms.ToolStripProgressBar();
+            this.stripSLEstatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pnlLogo = new System.Windows.Forms.Panel();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SStripLogin.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // SStripLogin
+            // 
+            this.SStripLogin.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stripPBEstatus,
+            this.stripSLEstatus});
+            this.SStripLogin.Location = new System.Drawing.Point(0, 191);
+            this.SStripLogin.Name = "SStripLogin";
+            this.SStripLogin.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.SStripLogin.Size = new System.Drawing.Size(475, 22);
+            this.SStripLogin.TabIndex = 0;
+            this.SStripLogin.Text = "statusStrip1";
+            // 
+            // stripPBEstatus
+            // 
+            this.stripPBEstatus.Name = "stripPBEstatus";
+            this.stripPBEstatus.Size = new System.Drawing.Size(100, 16);
+            // 
+            // stripSLEstatus
+            // 
+            this.stripSLEstatus.Name = "stripSLEstatus";
+            this.stripSLEstatus.Size = new System.Drawing.Size(16, 17);
+            this.stripSLEstatus.Text = "...";
+            // 
+            // pnlLogo
+            // 
+            this.pnlLogo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pnlLogo.BackColor = System.Drawing.Color.White;
+            this.pnlLogo.Location = new System.Drawing.Point(0, 0);
+            this.pnlLogo.Name = "pnlLogo";
+            this.pnlLogo.Size = new System.Drawing.Size(210, 191);
+            this.pnlLogo.TabIndex = 4;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUsuario.Location = new System.Drawing.Point(219, 38);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(244, 25);
+            this.txtUsuario.TabIndex = 0;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPass.Location = new System.Drawing.Point(219, 96);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(244, 25);
+            this.txtPass.TabIndex = 1;
+            this.txtPass.UseSystemPasswordChar = true;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnIniciar.Location = new System.Drawing.Point(243, 141);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(80, 30);
+            this.btnIniciar.TabIndex = 2;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSalir.Location = new System.Drawing.Point(354, 141);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(80, 30);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Usuario:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Contraseña:";
             // 
             // FrmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(393, 219);
+            this.ClientSize = new System.Drawing.Size(475, 213);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.pnlLogo);
+            this.Controls.Add(this.SStripLogin);
             this.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmLogin";
+            this.Load += new System.EventHandler(this.FrmLogin_Load);
+            this.SStripLogin.ResumeLayout(false);
+            this.SStripLogin.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip SStripLogin;
+        private System.Windows.Forms.ToolStripProgressBar stripPBEstatus;
+        private System.Windows.Forms.ToolStripStatusLabel stripSLEstatus;
+        private System.Windows.Forms.Panel pnlLogo;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- EXPOCOMA/login/FrmLogin.cs
+++ EXPOCOMA/login/FrmLogin.cs
@@ -1,19 +1,187 @@
-﻿using System;
+﻿using EXPOCOMA.Funciones;
+using EXPOCOMA.inicio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.DirectoryServices;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace EXPOCOMA.login
 {
     public partial class FrmLogin : Form
     {
+        funcionesdblocal _funciones = new funcionesdblocal();
+        public Boolean _ejecutando; //true = EJECUTANDO EL SISTEMA. false = SISTEMA EN EJECUCIÓN.
+      
+        private Thread CargarInfo;
+        private Int32 totalArchi = 0;
+        private Int32 TotalDll = 0;
+        public Boolean _respuestaLogin = false;
         public FrmLogin()
         {
             InitializeComponent();
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+            _funciones.icono(this);
+            FondoPantalla();
+            if (_ejecutando)
+            {
+                CargarInfo = new Thread(CargarInformacion);
+                CargarInfo.IsBackground = true;
+                CargarInfo.Start();
+            }
+            
+            
+            //frmInicio _frmInicio = new frmInicio();
+            //_frmInicio._respuestaLogin = true;
+
+
+
+        }
+
+        public void FondoPantalla()
+        {
+            //Ruta donde se encuentra nuestra imagen
+            string ruta = Application.StartupPath + @"\recursos\EXPO_ COMA.JPG";
+            MdiClient ctlMDI;
+            // Loop through all of the form's controls looking
+            // for the control of type MdiClient.
+            foreach (Control ctl in pnlLogo.Controls)
+            {
+                try
+                {
+                    // Attempt to cast the control to type MdiClient.
+                    ctlMDI = (MdiClient)ctl;
+
+                    // Set the BackColor of the MdiClient control.
+                    ctlMDI.BackColor = pnlLogo.BackColor;
+                }
+                catch (InvalidCastException exc)
+                {
+                    // Catch and ignore the error if casting failed.
+                }
+            }
+
+            //Comprobamos que la ruta exista
+            if (File.Exists(ruta))
+            {
+                //Creamos un Bitmap con la imagen
+                Bitmap bmp = new Bitmap(ruta);
+
+                //Se la colocamos de fondo al formulario
+                pnlLogo.BackgroundImage = bmp;
+                //pnlLogo.BackgroundImageLayout = ImageLayout.Center;
+                //this.BackgroundImageLayout = ImageLayout.Zoom;
+                pnlLogo.BackgroundImageLayout = ImageLayout.Stretch;
+                //pnlLogo.BackgroundImageLayout = ImageLayout.Tile;
+                //pnlLogo.BackgroundImageLayout = ImageLayout.None;
+            }
+        }
+
+        void  CargarInformacion()
+        {
+            _funciones.DesabilitarControles(this, false);
+            stripSLEstatus.Text = "Analizando";
+            Thread.Sleep(500);
+
+            String[] Archi = {
+                "sqlceca40.dll", "sqlcecompact40.dll","sqlceer40EN.dll","sqlceer40ES.dll", "sqlceme40.dll", "sqlceoledb40.dll","sqlceqp40.dll","sqlcese40.dll", "System.Data.SqlServerCe.dll", "dbexpo.sdf",
+                "Microsoft.SqlServer.Types.dll", "Microsoft.ReportViewer.Common.dll","Microsoft.ReportViewer.ProcessingObjectModel.DLL","Microsoft.ReportViewer.WinForms.DLL",
+                "EPPlus.dll","BarcodeFree.dll","zh-Hant\\Microsoft.SqlServer.Types.resources.dll","zh-Hans\\Microsoft.SqlServer.Types.resources.dll","ru\\Microsoft.SqlServer.Types.resources.dll",
+                "pt\\Microsoft.SqlServer.Types.resources.dll","ko\\Microsoft.SqlServer.Types.resources.dll","ja\\Microsoft.SqlServer.Types.resources.dll","it\\Microsoft.SqlServer.Types.resources.dll","fr\\Microsoft.SqlServer.Types.resources.dll",
+                "ES\\Microsoft.SqlServer.Types.resources.dll","ES\\System.Data.SqlServerCe.resources.dll","de\\Microsoft.SqlServer.Types.resources.dll","recursos\\comita.ico","recursos\\EXPO_ COMA.JPG",
+                "recursos\\loader.gif"
+            };
+            TotalDll = Archi.Count();
+
+            for (int i = 0; i < TotalDll; i++)
+            {
+                _funciones.Cargando(this, stripPBEstatus, 5, i, TotalDll, stripSLEstatus, "" + Archi[i].ToString());
+                if (!(File.Exists(Archi[i].ToString())))
+                {
+
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        MessageBox.Show("No encontre el archivo " + Archi[i].ToString(), "¡Upssss!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        _respuestaLogin = false;
+                        //Application.Exit();
+                        this.Close();
+                    });
+
+                    break;
+
+                }
+                else
+                {
+
+                    totalArchi++;
+
+                }
+                _funciones.Cargando(this, stripPBEstatus, 5, i + 1, TotalDll, stripSLEstatus, "" + Archi[i].ToString());
+            }
+
+            //Thread.Sleep(500);
+            try
+            {
+                
+                stripSLEstatus.Text = "Listo";
+                _funciones.DesabilitarControles(this, true);
+                
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+
+            this.Invoke((MethodInvoker)delegate
+                        {
+                            txtUsuario.Focus();
+                        });
+            
+            
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            DirectoryEntry directoryEntry = new DirectoryEntry("LDAP://servdc2", txtUsuario.Text, txtPass.Text);
+            DirectorySearcher searcher = new DirectorySearcher(directoryEntry)
+            {
+                PageSize = int.MaxValue,
+                //Filter = cadena.ToString(),
+                //Filter = ("samAccountName=programacion"),
+
+            };
+
+            SearchResult result = null;
+            try
+            {
+                result = searcher.FindOne();
+                _respuestaLogin = true;
+                this.Close();
+            }
+            catch (Exception ee)
+            {
+                //stripSLEstatus.Text = ee.Message;
+                stripSLEstatus.Text = "Usuario o contraseña incorrecta";
+
+                //MessageBox.Show(ee.Message);
+                _respuestaLogin = false;
+                //this.Close();
+            }
         }
     }
 }
